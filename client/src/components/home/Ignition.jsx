52708624b2980769f4f95f8b51c9adebeaabd11e
ElizaldeJE <javelisol@gmail.com>
2018-04-10 12:40:27 -0500

    created boiler plate with test showing in the main page. Connected to the server and it transpiling the files correctlty

---
+++ client/src/components/home/Ignition.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Test = () => (
+
+    <div>
+      <h1>This is a test 2.0</h1>
+    </div>
+
+);
+
+export default Test;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,11 +2,8 @@ import React from "react";
 import $ from "jquery";
 import { Link } from "react-router-dom";
 
-import Header from "../components/home/Header.jsx";
-import TypeWriteR from "../components/home/typewriter.jsx";
-import FoodMenu from "../components/home/FoodMenu.jsx";
-import Video from "../components/home/Video.jsx";
-import Carousel from "../components/home/Carousel.jsx";
+import Test from "../components/home/Ignition.jsx";
+
 
 class Home extends React.Component {
   constructor(props) {
@@ -18,27 +15,7 @@ class Home extends React.Component {
     return (
       <div>
         <div>
-          <Header />
-        </div>
-            <br/>
-            <br/>
-        <div>
-          <TypeWriteR />
-        </div>
-            <br/>
-            <br/>
-        <div>
-          <FoodMenu />
-        </div>
-        <div>
-        </div>
-        <div>
-          <Carousel />
-        </div>
-            <br/>
-            <br/>
-        <div>
-          <center><Video /></center>
+          <Test />
         </div>
       </div>
     );
